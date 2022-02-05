8962bf828d75bdde106aebfcd9afe51726ec4543
nonaybay <rafaelvenancio@protonmail.com>
2022-02-05 14:11:51 -0300

    Install Aurora Store

--- customize.sh
+++ customize.sh
@@ -2,4 +2,8 @@ if $BOOTMODE; then
 	echo ""
 	echo "Installing: Aurora Services";
 	pm install --dont-kill -g "$MODPATH/system/priv-app/AuroraServices/AuroraServices.apk"
+
+	echo ""
+	echo "Installing: Aurora Store";
+	pm install --dont-kill -g "$MODPATH/system/priv-app/AuroraStore/AuroraStore.apk"
 fi
