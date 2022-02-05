035531e0e4d9d7a0d9de0f33f17ae41b9e80c6a4
nonaybay <rafaelvenancio@protonmail.com>
2022-02-05 15:24:15 -0300

    Added Keyboard

--- customize.sh
+++ customize.sh
@@ -1,7 +1,19 @@
 if $BOOTMODE; then
 	echo "Installing app store (alternative to Google Play Store)"
 	pm install -g "$MODPATH/system/priv-app/AuroraStore/AuroraStore.apk"
+	echo ""
+	echo ""
+	echo ""
 
 	echo "Installing launcher"
-	pm install -g "$MODPATH/system/system_ext/priv-app/Launcher3QuickStep/Launcher3QuickStep.apk"
+	pm install -g "$MODPATH/system/priv-app/OmegaLauncher/OmegaLauncher.apk"
+	echo ""
+	echo ""
+	echo ""
+
+	echo "Installing keyboard"
+	pm install -g "$MODPATH/system/product/app/org.pocketworkstation.pckeyboard/org.pocketworkstation.pckeyboard.apk"
+	echo ""
+	echo ""
+	echo ""
 fi
