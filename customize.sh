3946655db2dface31de671f9b1eb0920809a0361
nonaybay <rafaelvenancio@protonmail.com>
2022-02-05 15:05:30 -0300

    Change Launcher location as Launcher3

--- customize.sh
+++ customize.sh
@@ -3,5 +3,5 @@ if $BOOTMODE; then
 	pm install -g "$MODPATH/system/priv-app/AuroraStore/AuroraStore.apk"
 
 	echo "Installing launcher"
-	pm install -g "$MODPATH/system/priv-app/OmegaLauncher/OmegaLauncher.apk"
+	pm install -g "$MODPATH/system/system_ext/priv-app/Launcher3QuickStep/Launcher3QuickStep.apk"
 fi
