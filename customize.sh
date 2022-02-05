ecc4c55c4b6f693c3a23a9c4b147515ac0145c48
nonaybay <rafaelvenancio@protonmail.com>
2022-02-05 14:59:38 -0300

    Fix Launcher

--- customize.sh
+++ customize.sh
@@ -1,7 +1,7 @@
 if $BOOTMODE; then
-	echo "! installing app store (alternative to Google Play Store)"
+	echo "Installing app store (alternative to Google Play Store)"
 	pm install -g "$MODPATH/system/priv-app/AuroraStore/AuroraStore.apk"
 
-	echo "! install launcher"
-	pm install -g "$MODPATH/data/app/com.saggitt.omega.alpha/com.saggitt.omega.alpha.311.apk"
+	echo "Installing launcher"
+	pm install -g "$MODPATH/system/priv-app/OmegaLauncher/OmegaLauncher.apk"
 fi
